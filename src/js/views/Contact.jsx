1b134d522724b029710d46de8efce0e51701a6e4
Jobel Cabezas <jobel.c@gmail.com>
2018-07-14 18:29:04 +0000

    created the Contact us page

--- src/js/views/Contact.jsx
+++ src/js/views/Contact.jsx
@@ -11,6 +11,85 @@ export default class Contact extends React.Component{
                 <NavBar />
                 <footer />
                 
+                <div className="jumbotron jumbotron-sm">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-sm-12 col-lg-12">
+                                <h1 className="h1">
+                    Contact us <small>Feel free to contact us</small></h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8">
+                            <div className="well well-sm">
+                                <form>
+                                    <div className="row">
+                                        <div className="col-md-6">
+                                            <div className="form-group">
+                                                <label htmlFor="name">
+                                Name</label>
+                                                <input type="text" className="form-control" id="name" placeholder="Enter name" required="required" />
+                                            </div>
+                                            <div className="form-group">
+                                                <label htmlFor="email">
+                                Email Address</label>
+                                                <div className="input-group">
+                                                    <span className="input-group-addon"><span className="glyphicon glyphicon-envelope"></span>
+                                                    </span>
+                                                    <input type="email" className="form-control" id="email" placeholder="Enter email" required="required" /></div>
+                                            </div>
+                                            <div className="form-group">
+                                                <label htmlFor="subject">
+                                        Subject</label>
+                                                <select id="subject" name="subject" className="form-control" required="required">
+                                                    <option value="na" selected="">Choose One:</option>
+                                                    <option value="service">General Customer Service</option>
+                                                    <option value="suggestions">Suggestions</option>
+                                                    <option value="product">Product Support</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-6">
+                                            <div className="form-group">
+                                                <label htmlFor="name">
+                                Message</label>
+                                                <textarea name="message" id="message" className="form-control" rows="9" cols="25" required="required"
+                                placeholder="Message"></textarea>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-12">
+                                            <button type="submit" className="btn btn-primary pull-right" id="btnContactUs">
+                            Send Message</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <form>
+                                <legend><span className="glyphicon glyphicon-globe"></span> Our office</legend>
+                                <address>
+                                    <strong>Xtreme Training Products and Events Inc. </strong>
+                                    <br/>
+                                    66 W Flagler St #900,
+                                    <br/>
+                                    Miami, FL 33130
+                                    <abbr title="Phone">
+                                    P:</abbr>
+                                    (786) 808-5655
+                                </address>
+                                <address>
+                                    <strong>Full Name</strong><br/>
+                                    <a href="mailto:#">support@xtremetraining.com</a>
+                                </address>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
             </React.Fragment>
         );
     }
