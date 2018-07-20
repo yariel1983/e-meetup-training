e0668e7131579a10e1c1577aace1eb1db5cc742c
Jesus <jpironag@gmail.com>
2018-07-20 00:27:36 +0000

    collapse navbar and session

--- src/js/component/NavBar.jsx
+++ src/js/component/NavBar.jsx
@@ -32,22 +32,23 @@ class Navbar extends React.Component{
                 <div>
                     <nav className="navbar navbar-expand-lg navbar-toggler-sm navbar-light bg-white">
                         <img src="http://www.hertsmereleisure.co.uk/centre_uploads/1/images/HM%20-%20Training%20Academy%20logo.jpg" width="150 px" height="60 px"/>
-                        <div className="container">
-                            <form className="form-inline my-2 my-lg-0">
-                                <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                                <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
-                                
-                            </form>
-                        </div>   
-                        <div className="navbar-brand" id="navbarNavAltMarkup">
-                            <div className="navbar-nav">
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <div className="navbar-nav mr-auto">
+                                <div className="container">
+                                    <form className="form-inline my-2 my-lg-0">
+                                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                                        <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+                                    </form>
+                                </div>                              
                                 <Link to={"/"} className="nav-item nav-link active" >Home <span className="sr-only">(current)</span></Link>
                                 <Link to={"/about"} className="nav-item nav-link" >About</Link>
                                 <Link to={"/products"} className="nav-item nav-link" >Products</Link>
                                 <Link to={"/training"} className="nav-item nav-link" >Trainings</Link>
                                 <Link to={"/events"} className="nav-item nav-link" >Events</Link>
                                 <Link to={"/contact"} className="nav-item nav-link" >Contacts</Link>
-                                
                                 {
                                     session && typeof(session.user_nicename) !== 'undefined' ?
                                         
@@ -68,7 +69,6 @@ class Navbar extends React.Component{
                                             data-target="#exampleModal">Login</button>
                                 }
     
-                                
                             </div>
                         </div>
                 
