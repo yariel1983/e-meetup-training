23011f3a420e4c5efc6a2d4ea4e6afa78b7a7141
Yariel <cubanboy4life@gmail.com>
2018-08-05 02:43:51 +0000

    page-about styling cards

--- src/js/component/PageAbout.jsx
+++ src/js/component/PageAbout.jsx
@@ -3,22 +3,64 @@ import 'bootstrap';
 import { Link } from "react-router-dom";
 
 function PageAbout(props){
-return (
-    <div className="jumbotron jumbotron-fluid bg-secondary mb-5 mt-5 border border-dark">
-        <div className="container mt-5 ">&nbsp;
-            <img className="card-img-top" id="imgabout" src="https://i.pinimg.com/originals/8f/72/d3/8f72d3e974b6b3a0fd14f1cdb53955eb.jpg" alt="Card image cap"/>
-            <h1 className="display-4">About us</h1>
-            <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
-    </div>
-        
-            
-
+        return (
+            <div>
+                <div className="container-fluid mt-5">&nbsp;
+                    <img className="card-img-top" id="imgabout" src="https://i.pinimg.com/originals/8f/72/d3/8f72d3e974b6b3a0fd14f1cdb53955eb.jpg" alt="Card image cap"/>
+                    <h1 className="display-4"></h1>
+                    <div className="container-fluid mt-5" id="cnt1">
+                        <div className="row feature">
+                            <div className="col-md-4">
+                                <div>
+                                    <img src="https://ae01.alicdn.com/kf/HTB1h0DtQVXXXXaRXpXXq6xXFXXXu/beibehang-Custom-photo-wallpaper-wall-murals-wall-stickers-3D-fitness-place-gym-fitness-fitness-beauty-tooling.jpg_640x640.jpg" alt="Texto Alternativo" className="img-circle img-thumbnail"/>
+                                    <h2>Admin Portal</h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam consequat, urna id vestibulum viverra, 
+                                        tellus felis rutrum erat, sit amet ultrices orci urna sit amet nunc. 
+                                        Fusce ut fringilla sem. Curabitur eget est vitae nulla eleifend ullamcorper et laoreet odio. 
+                                        Duis condimentum feugiat sollicitudin. Sed non lacinia turpis. 
+                                        Proin gravida turpis consequat porta condimentum. 
+                                        Nam consequat ultricies elit nec consectetur.</p>          
+                                    <a href="#" className="btn btn-success lower">See Feature details</a>
+                                </div>
+                            </div>
+                                        
+                            <div className="col-md-4">
+                                <div>
+                                    <img src="https://ae01.alicdn.com/kf/HTB12Q.USXXXXXataFXXq6xXFXXXb/zel-Foto-raf-Duvar-Ka-d-3D-Boks-Spor-Salonu-Arka-Plan-Afi-Resimleri-Papel.jpg_640x640q90.jpg" alt="Texto Alternativo" className="img-circle img-thumbnail"/>
+                                    <h2>Project Portfolio</h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam consequat, urna id vestibulum viverra, 
+                                        tellus felis rutrum erat, sit amet ultrices orci urna sit amet nunc. 
+                                        Fusce ut fringilla sem. Curabitur eget est vitae nulla eleifend ullamcorper et laoreet odio. 
+                                        Duis condimentum feugiat sollicitudin. Sed non lacinia turpis. 
+                                        Proin gravida turpis consequat porta condimentum. 
+                                        Nam consequat ultricies elit nec consectetur.</p> 
+                                    <a href="#" className="btn btn-success lower">See Feature details</a>
+                                </div>
+                            </div>
+                                        
+                            <div className="col-md-4">
+                                <div>
+                                    <img src="https://ae01.alicdn.com/kf/HTB1K..qPXXXXXctXXXXq6xXFXXXt/3D-photo-wallpaper-Bodybuilding-Sexy-Beauty-Gym-Photo-Wall-Bar-Backdrop-home-decoration-custom-mural-high.jpg_640x640.jpg" alt="Texto Alternativo" className="img-circle img-thumbnail"/>
+                                    <h2>Personal Website</h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam consequat, urna id vestibulum viverra, 
+                                        tellus felis rutrum erat, sit amet ultrices orci urna sit amet nunc. 
+                                        Fusce ut fringilla sem. Curabitur eget est vitae nulla eleifend ullamcorper et laoreet odio. 
+                                        Duis condimentum feugiat sollicitudin. Sed non lacinia turpis. 
+                                        Proin gravida turpis consequat porta condimentum. 
+                                        Nam consequat ultricies elit nec consectetur.</p>
+                                    <a href="#" className="btn btn-success lower">See Feature details</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
     );
 }
 
 export default PageAbout;
                     
+
                                 
                     
                         
